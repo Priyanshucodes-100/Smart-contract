26415ae8ed0a362d16b2dec19aec8e83c198a286
Priyanshucodes-100 <priyanshukumar12983@gmail.com>
2026-06-12 17:39:28 +0530

    Add modifiersol contract

--- Modifier.sol
+++ Modifier.sol
@@ -9,7 +9,9 @@ contract Modifier{
     constructor(){
         owner = msg.sender;
     }
-
+    //modifier are code that can be run before and after the function call
+    //generally used for 3 reason
+    //Restrict access, validate inputs, guard against reentrancy hack.
     modifier onlyowner(){
         require(msg.sender == owner, "Not owner");
         _;
@@ -23,4 +25,37 @@ contract Modifier{
     function changeowner(address _newowner)public onlyowner validatedata(_newowner){
         owner = _newowner;
     }
+
+    modifier noReentrancy(){
+        require(!locked, " noreenterancy");
+        locked =true;
+        _;
+        locked = false;
+    }
+
+    function decrement(uint256 i)public noReentrancy{
+        x -= 1;
+
+        if(i>1){
+            decrement(i-1);
+        }
+    }
 }
+//modifiers
+// Modifier is used to restrict access to functions.
+
+//  _ ; represents the function body.
+// _; this means that the next line will execute only after running this line.
+
+//  One modifier can be used by many functions.
+
+//  Modifiers can have parameters.
+
+//  Multiple modifiers can be applied to a function.
+
+//  Commonly used for:
+
+// Ownership checks
+// Access control
+// Input validation
+// Pausing contracts
